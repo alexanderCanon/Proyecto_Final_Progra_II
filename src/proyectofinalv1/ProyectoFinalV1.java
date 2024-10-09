a3a0089a58e90ecfd9eca73f13d52433b2ac0a0d
alexanderCanon <alexandercanon64@gmail.com>
2024-10-08 22:09:14 -0600

    Segundo commit

--- src/proyectofinalv1/ProyectoFinalV1.java
+++ src/proyectofinalv1/ProyectoFinalV1.java
@@ -4,7 +4,8 @@ package proyectofinalv1;
 public class ProyectoFinalV1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Holaaaaaa p");
+        
     }
     
 }
