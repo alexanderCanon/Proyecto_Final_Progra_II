8ce2786e33f01e58ed9c80407602c13e772ffcf2
alexanderCanon <160709836+alexanderCanon@users.noreply.github.com>
2024-10-08 22:12:43 -0600

    Tercer commit

--- src/proyectofinalv1/ProyectoFinalV1.java
+++ src/proyectofinalv1/ProyectoFinalV1.java
@@ -6,6 +6,7 @@ public class ProyectoFinalV1 {
     public static void main(String[] args) {
         System.out.println("Holaaaaaa p");
         System.out.println("Hola dsl");
+        System.out.println("Otro co");
     }
     
 }
